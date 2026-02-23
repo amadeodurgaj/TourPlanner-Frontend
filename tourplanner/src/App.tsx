5d92803e4f3558e9949d7a5cbb7d19087b364cdf
Amadeo Durgaj <thundahlovesjojo@gmail.com>
2026-02-23 18:47:26 +0100

    Basic API Setup for communication

--- tourplanner/src/App.tsx
+++ tourplanner/src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import { apiGet } from "./api/ApiClient";
+export default function App() {
+    const [message, setMessage] = useState("");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        apiGet("/hello")
+            .then((data) => setMessage(data.message))
+    }, []);
+
+    return (
+        <div>
+            <h1>Frontend</h1>
+            <p>{message}</p>
+        </div>
+    );
 }
 
-export default App;
---
+++ tourplanner/src/api/ApiClient.ts
@@ -0,0 +1,11 @@
+const API_URL = process.env.REACT_APP_API_URL;
+
+export const apiGet = async (endpoint: string) => {
+    const response = await fetch(`${API_URL}${endpoint}`);
+
+    if (!response.ok) {
+        throw new Error("API request failed");
+    }
+
+    return response.json();
+};
